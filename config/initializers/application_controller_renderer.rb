1a4c15b72bc98c26f822f84a4c32238abe85fd18
sagae keiga <sagae5.28rujeae@gmail.com>
2017-03-01 02:59:10 +0000

    first

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# ApplicationController.renderer.defaults.merge!(
+#   http_host: 'example.org',
+#   https: false
+# )
+ActiveSupport.halt_callback_chains_on_return_false = false
